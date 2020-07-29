ca299a199461d7c5fa105a2a2e8bfc015a703e1e
Eduardo Castillo <ecm1512@hotmail.com>
2020-07-28 20:25:37 -0500

    Añadi campos nuevos a la tabla usuario, subi 3 sp y actualice el cronograma

---
+++ desarrollo/SW_ModeloDeCalidad-ISO9126/4. Fuentes/PROC_login.sql
@@ -0,0 +1,20 @@
+CREATE DEFINER=`swmciso9126`@`%` PROCEDURE `PROC_login`(
+	IN userval VARCHAR(50),
+    IN passwordval VARCHAR(50),
+    OUT code_rsp INT,
+    OUT message_rsp VARCHAR(50)
+)
+BEGIN
+  declare flag INTEGER;
+  
+  SELECT COUNT(0) INTO flag FROM Cuenta cu WHERE cu.user = userval AND cu.password = md5(passwordval);
+  
+  IF flag = 1 THEN
+    SET code_rsp = 0;
+    SET message_rsp = 'INICIO DE SESION CORRECTO';
+  ELSE
+    SET code_rsp = 1;
+    SET message_rsp = 'ERROR';
+  END IF;
+  
+END
