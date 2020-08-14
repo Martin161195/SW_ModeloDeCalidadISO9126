3c617f98a53e8ad400ccb3a2ed4a56a377addc2a
Eduardo Castillo <ecm1512@hotmail.com>
2020-08-14 16:27:30 -0500

    Aumente la data de las metricas

--- desarrollo/SW_ModeloDeCalidad-ISO9126/4. Fuentes/data.sql
+++ desarrollo/SW_ModeloDeCalidad-ISO9126/4. Fuentes/data.sql
@@ -71,3 +71,80 @@ insert into Metrica values (18,8,3,'Suficiencia del protocolo de intercambio de 
 insert into Metrica values (19,8,3,'Adecuacion de interfaz externa');
 
 
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (9,4,'Descripcion completa de los escenarios');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (9,4,'Cobertura de demostracion');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (9,4,'Autodescripcion del punto de entrada');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (10,4,'Completitud de la guia de usuario');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (10,4,'Formularios con valores por defecto');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (10,4,'Mensajes de error entendibles');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (10,4,'Interfaces de usuario intuitivas');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (11,4,'Consistencia operacional');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (11,4,'Claridad del mensaje');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (11,4,'Customizacion funcional');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (11,4,'Customizacion de la interface de usuario');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (11,4,'Capacidad de monitoreo');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (11,4,'Capacidad de deshacer acciones');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (11,4,'Categorizacion de la informacion entendible');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (11,4,'Consistencia de la apariencia de las interfaces de usuario');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (11,4,'Soporte de dispositivos de entrada');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (12,4,'Evitar errores de operación del usuario');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (12,4,'Corrección de errores de entrada de usuario');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (12,4,'Recuperabilidad de errores del usuario');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (13,4,'Apariencia estética de las interfaces de usuario');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (14,4,'Accesibilidad para usuarios con discapacidad');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (14,4,'Adecuación de los idiomas admitidos');
+
+
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (15,5,'Correccion de fallas');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (15,5,'Tiempo medio entre fallos');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (15,5,'Tasa de fallos');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (15,5,'Cobertura de prueba');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (16,5,'Disponibilidad del sistema');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (16,5,'Tiempo de inactividad despues de un fallo');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (17,5,'Evitacion de fallas');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (17,5,'Redundancia de componentes');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (17,5,'Tiempo de notificacion de errores');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (18,5,'Tiempo medio de recuperacion');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (18,5,'Integridad de los datos de respaldo');
+
+
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (19,6,'Control de acceso');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (19,6,'Correctitud del cifrado de datos');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (19,6,'Fuerza de los algoritmos criptográficos');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (20,6,'Integridad de datos');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (20,6,'Prevencion de la corrupcion de datos internos');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (20,6,'Prevención de desbordamiento de búfer');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (21,6,'Uso de firma digital');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (22,6,'Integridad de la pista de auditoría del usuario');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (22,6,'Retencion del log del sistema');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (23,6,'Suficiencia del mecanismo de autenticación');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (23,6,'Conformidad de las reglas de autenticacion');
+
+
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (24,7,'Acoplamiento de componentes');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (24,7,'Adecuacion de la complejidad ciclomatica');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (25,7,'Reusabilidad de los activos');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (25,7,'Conformidad de las reglas de codificacion');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (26,7,'Completitud del log del sistema');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (26,7,'Efectividad de la función de diagnóstico');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (26,7,'Suficiencia de la función de diagnóstico');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (27,7,'Eficiencia de la modificacion');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (27,7,'Correccion de la modificacion');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (27,7,'Capacidad de la modificacion');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (28,7,'Integridad de la función de prueba');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (28,7,'Testabilidad autónoma');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (28,7,'Prueba de reiniciabilidad');
+
+
+
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (29,8,'Adaptabilidad ambiental del hardware');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (29,8,'Adaptabilidad ambiental del software del sistema');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (29,8,'Adaptabilidad ambiental operativa');
+
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (30,8,'Eficiencia del tiempo de instalacion');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (30,8,'Facilidad de instalacion');
+
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (31,8,'Similitud de uso');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (31,8,'Equivalencia de calidad del producto');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (31,8,'Inclusividad funcional');
+insert into Metrica(idSubcaracteristica,idCaracteristica,nombre) values (31,8,'Capacidad de reutilización / importación de datos');
